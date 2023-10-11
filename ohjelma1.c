c3a09ff8e9eaddb59aa17375e9d27ea399135c94
Heikkila, Juha-pekka <juha-pekka.heikkila@intel.com>
2023-10-11 15:17:09 +0300

    ohjema1.c

---
+++ ohjelma1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+Tulostaa
+       a
+      aa
+     aaa
+    aaaa
+   aaaaa
+  aaaaaa
+ aaaaaaa
+aaaaaaaa
+*/
+
+int main() {
+	int i, j, n = 8;
+
+	for(i = 1; i <= n; i++) {
+		for(j = 1; j <= n - i; j++) {
+			printf(" ");
+		}
+		for(j = 1; j <= i; j++) {
+			printf("a");
+		}
+		printf("\n");
+	}
+	return 0;
+}
