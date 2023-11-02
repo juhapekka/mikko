a84bdab3a0a4ae04939a21ab58f4b58a55fea0fe
Heikkila, Juha-pekka <juha-pekka.heikkila@intel.com>
2023-11-02 23:26:26 +0200

    taydellinen.c korjaus

--- taydellinen.c
+++ taydellinen.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int onkoLukuTaydellinen(int luku);
+
+int main(void) {
+    for (int i = 1; i <= 10000; i++) {
+        if (onkoLukuTaydellinen(i)) {
+            printf("\nLuku %d on täydellinen luku", i);
+        }
+    }
+    return 0;
+}
+
 int onkoLukuTaydellinen(int luku) {
     int summa = 0;
     for (int i = 1; i <= luku / 2; i++) { // Maksimitekijä on luku/2
@@ -10,15 +21,6 @@ int onkoLukuTaydellinen(int luku) {
 
     if (summa == luku) {
         return 1;
-    }
-    return 0;
-}
-
-int main(void) {
-    for (int i = 1; i <= 10000; i++) {
-        if (onkoLukuTaydellinen(i)) {
-            printf("Luku %d on täydellinen luku\n", i);
-        }
     }
     return 0;
 }
